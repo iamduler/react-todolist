3633053aec7291fa71305f5f480f3e2b97c51e2b
lam.bui <lam.bui@cloudgo.vn>
2025-06-04 22:04:11 +0700

    Sync data with local storage

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,8 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TaskInput from '../TaskInput/TaskInput'
 import TaskList from '../TaskList/TaskList'
 import styles from './todoList.module.scss'
 import { Todo } from '../../@types/todo.type'
+
+interface HandleNewTodo {
+  (todoObj: Todo[]): Todo[] // Function that takes an array of todos and returns an array of todos
+}
+
+// type HandleNewTodo = (todoObj: Todo[]) => Todo[]
+
+const syncLocalStorage = (handleNewTodo: HandleNewTodo) => {
+  const todoString = localStorage.getItem('todos')
+  const todoObj: Todo[] = JSON.parse(todoString || '[]')
+  const newToDoObj = handleNewTodo(todoObj)
+  localStorage.setItem('todos', JSON.stringify(newToDoObj))
+}
 
 export default function TodoList() {
   const [todos, setTodos] = useState<Todo[]>([])
@@ -10,13 +23,26 @@ export default function TodoList() {
   const doneTodos = todos.filter((todo) => todo.done)
   const notDoneTodos = todos.filter((todo) => !todo.done)
 
+  useEffect(() => {
+    const todoString = localStorage.getItem('todos')
+    const todoObj: Todo[] = JSON.parse(todoString || '[]')
+    setTodos(todoObj)
+  }, [])
+
   const addTodo = (name: string) => {
     const newTodo: Todo = {
       id: new Date().toISOString(),
       name,
       done: false
     }
-    setTodos((prev) => [...prev, newTodo])
+
+    const handler = (todoObj: Todo[]) => {
+      return [...todoObj, newTodo]
+    }
+    setTodos(handler)
+
+    // Save to local storage
+    syncLocalStorage(handler)
   }
 
   const handleDoneTodo = (id: string, done: boolean) => {
@@ -45,24 +71,32 @@ export default function TodoList() {
   }
 
   const finishEditTodo = () => {
-    setTodos((prev) => {
-      return prev.map((todo) => {
+    const handler = (todoObj: Todo[]) => {
+      return todoObj.map((todo) => {
         if (todo.id === currentTodo?.id) {
           return currentTodo as Todo
         }
         return todo
       })
-    })
+    }
+    setTodos(handler)
     setCurrentTodo(null)
+
+    // Save to local storage
+    syncLocalStorage(handler)
   }
 
   const deleteTodo = (id: string) => {
     if (currentTodo?.id === id) {
       setCurrentTodo(null)
     }
-    setTodos((prev) => {
-      return prev.filter((todo) => todo.id !== id)
-    })
+    const handler = (todoObj: Todo[]) => {
+      return todoObj.filter((todo) => todo.id !== id)
+    }
+    setTodos(handler)
+
+    // Save to local storage
+    syncLocalStorage(handler)
   }
 
   return (
